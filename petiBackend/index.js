0f036accdf47670d07bdfe92800bbd1ba8246c8d
Nekgate <Nekgate@gmail.com>
2024-09-05 12:15:55 +0100

    First commit after the initial creation of index.js and database creation

---
+++ petiBackend/index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const connectDB = require("./database/db")
+const app=express()
+const dotenv=require("dotenv")
+
+dotenv.config()
+
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running")
+})
