eeeaec532dc5ff28707be1995a1e533e51566f8b
Nekgate <Nekgate@gmail.com>
2024-09-06 23:30:24 +0100

    Adding user to DB testing

--- petiBackend/models/User.js
+++ petiBackend/models/User.js
@@ -1,59 +1,59 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-  userName:{
-    type:String,
-    required:true,
-    unique:true,
-    trim:true,
-    lowercase:true
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true,
-    trim:true,
-    lowercase:true
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
   },
-  password:{
-    type:String,
-    required:true
+  password: {
+    type: String,
+    required: true
   },
-  fullName:{
-    type:String,
-    required:true,
-    trim:true
+  fullname: {
+    type: String,
+    required: true,
+    trim: true
   },
-  bio:{
-    type:String,
-    required:true
+  bio: {
+    type: String,
+    required: true
   },
-  profilePicture:{
-    type:String,
-    default:""
+  profilePicture: {
+    type: String,
+    default: ""
   },
-  coverPicture:{
-    type:String,
-    default:""
+  coverPicture: {
+    type: String,
+    default: ""
   },
-  posts:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Post"
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
   }],
-  followers:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }],
-  following:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }],
-  blocklist:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  blocklist: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }]
-},{timestamps:true})
+}, { timestamps: true });
 
-const User=mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports=User
+module.exports = User;
