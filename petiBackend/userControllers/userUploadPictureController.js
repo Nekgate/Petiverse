a1c67480188521317d4a9ebd7dc965849e1b2ba8
kingsantus <nkingsantus@gmail.com>
2024-09-11 21:19:47 +0100

    uploading image with cloudinary

--- petiBackend/userControllers/userUploadPictureController.js
+++ petiBackend/userControllers/userUploadPictureController.js
@@ -14,7 +14,7 @@ const uploadProfilePictureController = async (req, res, next) => {
         const imageUrl = req.imageUrl
         // check if cloudinary was able to upload and get url
         if (!imageUrl) {
-            throw new CustomError("Image was not uploaded", 500);
+            throw new CustomError("Image was not uploaded", 404);
         }
         // get the id of user from the verifiedToken of user in cookie
         const logId = req.userId;
@@ -45,16 +45,18 @@ const uploadProfilePictureController = async (req, res, next) => {
 }
 
 const uploadCoverPictureController = async (req, res, next) => {
-    // get the userId from url
-    const { userId } = req.params;
-    // extract the file uploaded from request file
-    const { filename } = req.file;
     try {
-        // get the userId from url
-        const { userId } = req.params;
-        // throw error if params is not available
-        if (!userId){
-            throw new CustomError("User Id is required", 400);
+        // extract the file uploaded from request file processed in multer
+        const { filename } = req.file;
+        // throw error if not found
+        if (!filename) {
+            throw new CustomError("Profile Picture is required", 400);
+        }
+        // get the image url of the file uploaded in cloudinary
+        const imageUrl = req.imageUrl
+        // check if cloudinary was able to upload and get url
+        if (!imageUrl) {
+            throw new CustomError("Image was not uploaded", 404);
         }
         // get the id of user from the verifiedToken of user in cookie
         const logId = req.userId;
@@ -62,16 +64,22 @@ const uploadCoverPictureController = async (req, res, next) => {
         if (!logId) {
             throw new CustomError("You have to login first", 401);
         }
+        // check if user exist
+        const user = await User.findById(logId);
+        // if the user not found throw error
+        if (!user) {
+            throw new CustomError("User not found", 401);
+        }
+        // throw error if user is not verified
+        if (!user.isVerified){
+            throw new CustomError("User does not exist", 403);
+        }
         // get the update the user object using the userId
         // updating only the generated file url in the file
         // new:true ensures it can create as many
-        const user = await User.findByIdAndUpdate(userId, {coverPicture:generateFileUrl(filename)},{new:true});
-        // if the user not found throw error
-        if (!user) {
-            throw new CustomError("User not found", 404);
-        }
-        res.status(200).json({message:"Cover picture updated successfully", user});
+        await User.findByIdAndUpdate(logId, {coverPicture:imageUrl},{new:true});
 
+        res.status(200).json({message:"Profile picture updated successfully"});
     } catch (error) {
         next(error);
     }
