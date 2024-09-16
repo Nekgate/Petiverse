03d3a906aea9d7e000a0f9d6a73f01f970568509
kingsantus <nkingsantus@gmail.com>
2024-09-16 13:49:37 +0100

    Case sensitive Image folder changed

--- petiFrontend/src/mocks/referrals.jsx
+++ petiFrontend/src/mocks/referrals.jsx
@@ -6,7 +6,7 @@ export const data = [
       "I can't express how much I love this platform! It’s not just a social network; it’s a community where pet lovers can truly connect. I've met so many wonderful people who share my passion for dogs, and the advice I’ve received has been invaluable. My pup, Max, is happier and healthier thanks to the tips I've learned here. Whether it's sharing adorable photos or getting quick answers to pet care questions, this site has it all!",
     reviewer: "__Emily R",
     title: "Proud Dog Owner",
-    img: "/images/tin-pic.png"
+    img: "/Images/tin-pic.png"
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const data = [
       "I can't express how much I love this platform! It’s not just a social network; it’s a community where pet lovers can truly connect. I've met so many wonderful people who share my passion for dogs, and the advice I’ve received has been invaluable. My pup, Max, is happier and healthier thanks to the tips I've learned here. Whether it's sharing adorable photos or getting quick answers to pet care questions, this site has it all!",
     reviewer: "__Racheal M.",
     title: "Proud Cat Owner",
-    img: "/images/tin-pic.png"
+    img: "/Images/tin-pic.png"
   },
   {
     id: 3,
@@ -24,6 +24,6 @@ export const data = [
       "I can't express how much I love this platform! It’s not just a social network; it’s a community where pet lovers can truly connect. I've met so many wonderful people who share my passion for dogs, and the advice I’ve received has been invaluable. My pup, Max, is happier and healthier thanks to the tips I've learned here. Whether it's sharing adorable photos or getting quick answers to pet care questions, this site has it all!",
     reviewer: "__Marvel H.",
     title: "Proud Giraff Owner",
-    img: "/images/tin-pic.png"
+    img: "/Images/tin-pic.png"
   },
 ];
