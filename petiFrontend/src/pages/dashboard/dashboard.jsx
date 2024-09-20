ac6771de6ddba18f5b5f4f82a63ce6128ee73b9e
lolade <ololade0507@gmail.com>
2024-09-20 10:02:07 +0100

    side-bar dashboard

--- petiFrontend/src/pages/dashboard/dashboard.jsx
+++ petiFrontend/src/pages/dashboard/dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import "../../pages/dashboard/dashboard.css";
 import Nav from "../../components/dashboard/nav/Nav";
+import SideBar from '../../components/dashboard/sideBar/SideBar';
 
 function dashboard() {
   return (
-    <div>
+    <div className='main-dash'>
       <Nav />
+      <div className="dash-grid">
+        <SideBar />
+      </div>
     </div>
   )
 }
