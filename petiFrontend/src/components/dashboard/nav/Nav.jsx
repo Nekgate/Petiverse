518c0f45dcbec57989e8219342b03a97cc1e1c9f
lolade <ololade0507@gmail.com>
2024-09-20 20:34:39 +0100

    side-ads-search

--- petiFrontend/src/components/dashboard/nav/Nav.jsx
+++ petiFrontend/src/components/dashboard/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../components/dashboard/nav/Nav.css";
 import { Link } from "react-router-dom";
 
--- petiFrontend/src/pages/dashboard/dashboard.jsx
+++ petiFrontend/src/pages/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "../../pages/dashboard/dashboard.css";
 import Nav from "../../components/dashboard/nav/Nav";
 import SideBar from '../../components/dashboard/sideBar/SideBar';
+import SearchPost from  "../../components/dashboard/searchPost/SearchPost";
+import Advert from "../../components/dashboard/advertisement/Advert";
 
 function dashboard() {
   return (
@@ -9,6 +11,10 @@ function dashboard() {
       <Nav />
       <div className="dash-grid">
         <SideBar />
+        <SearchPost />
+        <div className='dash-ad'>
+          <Advert />
+        </div>
       </div>
     </div>
   )
